89890dfb5e94fdad71bd5bf8d2306de08c6b21d9
George2811 <sanchez28112001@gmail.com>
2022-08-09 23:19:55 -0500

    added medicines controller

--- controllers/medicines.controller.js
+++ controllers/medicines.controller.js
@@ -1,4 +1,52 @@
 import { exceptionResponse } from "../exceptions/exceptionResponse.js";
 import { MedicinesService } from "../services/medicines.service.js";
 
+export const getAllMedicines = async (req, res) => {
+    try {
+        const medicines = await MedicinesService.getAllMedicines();
+        return res.status(200).json(medicines);
+    } catch (error) {
+        const { code, message } = exceptionResponse(error);
+        return res.status(code).json({ message });
+    }
+}
 
+export const getMedicinesByName = async (req, res) => {
+    try {
+        const medicines = await MedicinesService.getMedicinesByName(req.params.medicinesName);
+        return res.status(200).json(medicines); 
+    } catch (error) {
+        const { code, message } = exceptionResponse(error);
+        return res.status(code).json({ message });
+    }
+}
+
+export const createMedicines = async (req, res) => {
+    try {
+        const medicines = await MedicinesService.createMedicines(req.body);
+        return res.status(201).json(medicines); 
+    } catch (error) {
+        const { code, message } = exceptionResponse(error);
+        return res.status(code).json({ message });
+    }
+}
+
+export const updateMedicines = async (req, res) => {
+    try {
+        const medicines = await MedicinesService.updateMedicines(req.body, req.params.medicinesId);
+        return res.status(201).json(medicines); 
+    } catch (error) {
+        const { code, message } = exceptionResponse(error);
+        return res.status(code).json({ message });
+    }
+}
+
+export const deleteMedicines = async (req, res) => {
+    try {
+        const medicines = await MedicinesService.deleteMedicines(req.params.medicinesId);
+        return res.status(201).json(medicines); 
+    } catch (error) {
+        const { code, message } = exceptionResponse(error);
+        return res.status(code).json({ message });
+    }
+}
