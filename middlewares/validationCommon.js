0dee4529f06d8a6a4d76f50b2d4338b53f677cf6
DiegoAlonso0 <diegoalonsooliverab@gmail.com>
2022-10-31 17:54:00 -0500

    project setup with nest js

--- middlewares/validationCommon.js
+++
@@ -1,9 +0,0 @@
-import { validationResult, body, param } from "express-validator";
-
-export const validationResultExpress = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-}
